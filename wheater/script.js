0ae6631a677d4633452f7a25b6501265230ff2f5
SqueezeKigs <squeezedrunk100@gmail.com>
2024-05-15 10:56:07 -0500

    esta bien

---
+++ wheater/script.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+    let lon
+    let lat
+
+    if (navigator.geolocation) {
+
+    }
+
+
+    let url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={6dee64b86038f6176be2f26ec663ac2b}'
+})
